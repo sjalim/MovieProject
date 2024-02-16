ad1e32b792caa3186595bf0ef22c9a887532a60f
S. S. Saruar Jahan <os-sssaruar.jahan@rakuten.com>
2024-02-16 09:09:24 +0600

    init

---
+++ MovieProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieProject
+//
+//  Created by BJIT on 16/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieProject/MovieProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieProjectApp.swift
+//  MovieProject
+//
+//  Created by BJIT on 16/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
